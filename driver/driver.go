303a964ad3293697749c959a28ca7d40277a9069
Guest <ivanhk@stud.ntnu.no>
2018-03-08 14:17:48 +0100

    updated

--- driver/driver.go
+++ driver/driver.go
@@ -2,10 +2,15 @@ package driver
 
 import "fmt"
 
-func FSM(f <- chan int) {
+
+func FSM(f <- chan int, e Elevator) {
+	prev_floor := -1
 	for {
 		select {
 		case floor := <- f
+			if (floor != prev_floor) {
+				if (
+			}
 			
 			
 		}
