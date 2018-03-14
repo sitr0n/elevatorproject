52fc5e812af620bbd54ca67a7e8c4a00ea99a66d
linact <lctheima@stud.ntnu.no>
2018-03-14 21:12:11 +0100

    saving

--- state/state.go
+++ state/state.go
@@ -6,45 +6,45 @@ import ("fmt"
 )
 import order "../order"
 
-func SaveState(s <- chan bool, elevator *order.Elevator) {
+func SaveState(s <- chan bool, state *order.Elevator) {
 	for {
 		select {
 		case <- s :
 			fmt.Println("Saving state.")
-			fmt.Println(elevator.Stops)
 			
-			jsonState, err := json.Marshal(elevator)
-			if err != nil {
-				fmt.Println("Failed to convert elevator state to Marshal.")
-				panic(err)
-			}
+			jsonState, err := json.Marshal(state)
+			check(err)
 
 			err = ioutil.WriteFile("state/state.json", jsonState, 0644)
-			if err != nil {
-				fmt.Println("Failed to save state.")
-				panic(err)
-			}
-
+			check(err)
 		}
 	}
 }
 
-func LoadState(elevator *order.Elevator) {
-	//var jsonBlob = []byte(`{"dir":1,"currentfloor":2,"stops":[1,1,1,1]}`)
+func LoadState(state *order.Elevator) {
 	fmt.Println("--------------------------")
 	fmt.Println("Loading state:")
-	fmt.Println("Previous state:\t", elevator.Stops)
+	fmt.Println("Previous state:\t", state.Stops)
 	
-	jsonState, errr := ioutil.ReadFile("state/state.json")
-	if errr != nil {
-		panic(errr)
-	}
+	jsonState, err := ioutil.ReadFile("state/state.json")
+	check(err)
 	
-	err := json.Unmarshal(jsonState, &elevator)
-	if err != nil {
-		panic(err)
-	}
+	err = json.Unmarshal(jsonState, &state)
+	check(err)
 	
-	fmt.Println("New state:\t", elevator.Stops)
+	fmt.Println("New state:\t", state.Stops)
 	fmt.Println("--------------------------")
+}
+
+func LoadState_test(state *order.Elevator) {
+	var jsonBlob = []byte(`{"dir":1,"currentfloor":0,"stops":[1,1,1,1]}`)
+	
+	err := json.Unmarshal(jsonBlob, &state)
+	check(err)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
