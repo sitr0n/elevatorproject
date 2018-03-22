89dea7a7c2c70976ab91b856b8fb4f726e250531
danieta <danieta@github.com>
2018-03-22 16:05:34 +0100

    added listener folder

---
+++ listener/state/state.go
@@ -0,0 +1,59 @@
+package state
+
+import ("fmt"
+	"encoding/json"
+	"io/ioutil"
+)
+
+
+type MotorDirection int
+const (
+	MD_Up   MotorDirection = 1
+	MD_Down                = -1
+	MD_Stop                = 0
+)
+
+type Elevator struct {
+	Dir MotorDirection
+	CurrentFloor int
+	Stops [4]int
+}
+
+
+func Save(s <- chan bool, state *Elevator) {
+	for {
+		select {
+		case <- s :
+			fmt.Println("Saving state.")
+			
+			jsonState, err := json.Marshal(state)
+			Check(err)
+
+			err = ioutil.WriteFile("state/state.json", jsonState, 0644)
+			Check(err)
+		}
+	}
+}
+
+func Load(state *Elevator) {
+	fmt.Println("\nLoading data...\n")
+	
+	jsonState, err := ioutil.ReadFile("state/state.json")
+	Check(err)
+	
+	err = json.Unmarshal(jsonState, &state)
+	Check(err)
+}
+
+func LoadState_test(state *Elevator) {
+	var jsonBlob = []byte(`{"dir":1,"currentfloor":0,"stops":[1,1,1,1]}`)
+	
+	err := json.Unmarshal(jsonBlob, &state)
+	Check(err)
+}
+
+func Check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
