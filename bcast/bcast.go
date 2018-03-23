f22878d1b0116759fccf5a23312a6308be680885
linact <lctheima@stud.ntnu.no>
2018-03-23 16:11:19 +0100

    removed serialreader.png

--- bcast/bcast.go
+++ bcast/bcast.go
@@ -9,30 +9,30 @@ import ("bytes"
 import state "../state"
 
 const stasjon22 string = "129.241.187.56:0"
+const stasjon23 string = "129.241.187.57:10001"
 
-const localIP string = stasjon22
+const localIP string =	stasjon22
+const targetIP string = stasjon23
 
 func Broadcast(data *state.Elevator) {
-       
-	ServerAddr,err := net.ResolveUDPAddr("udp","129.241.187.57:10001")
-  	state.Check(err)
- 
-  	LocalAddr, err := net.ResolveUDPAddr("udp", localIP)
- 	state.Check(err)
- 
+
+	ServerAddr,err := net.ResolveUDPAddr("udp", targetIP)
+	state.Check(err)
+
+	LocalAddr, err := net.ResolveUDPAddr("udp", localIP)
+	state.Check(err)
+
 	connection, err := net.DialUDP("udp", LocalAddr, ServerAddr)
 	state.Check(err)
 
-        defer connection.Close()
-        
-        
-        
-        var buffer bytes.Buffer
-        encoder := gob.NewEncoder(&buffer)
+	defer connection.Close()
+
+	var buffer bytes.Buffer
+	encoder := gob.NewEncoder(&buffer)
+
 	
-        //message := <-state
-        encoder.Encode(data)
-        connection.Write(buffer.Bytes())
-        fmt.Println("Broadcasting: ", data)
-        buffer.Reset()
+	encoder.Encode(data)
+	connection.Write(buffer.Bytes())
+	fmt.Println("Broadcasting: ", *data)
+	//buffer.Reset()
 }
